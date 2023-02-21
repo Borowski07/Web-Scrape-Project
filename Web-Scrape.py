96242d940546f2e9fbcecd2cd901c52f1c447e92
Matt <borowski07@hotmail.com>
2023-02-21 12:07:08 +1100

    selenium initial setup

--- Web-Scrape.py
+++ Web-Scrape.py
@@ -1,8 +1,27 @@
-#test1
+# Author:  Matthew Borowski
+# Date Created: Monday 20/02/2023 16:10 
+# Project Name: Detecting non-compliance therapeutic goods selling on e-commerce websites.
+# Project Group ID: 9785-23-06
 
 
-#test2
+#%% Initialise packages etc.
 
-#test 3
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+#%% Selenium Driver Setup (chrome window)
+
+URL= "https://www.amazon.com.au/"
+
+#compile webdriver / browser for selenium to run
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
 
 
+
+
+# %%

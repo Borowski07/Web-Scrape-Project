e0940a09063c1b3ca981617031d5122af42065a5
Matthew Borowski <9100606+Borowski07@users.noreply.github.com>
2023-03-15 17:56:05 +1100

    update to infromation collection

--- Web-Scrape.py
+++ Web-Scrape.py
@@ -30,6 +30,9 @@ URL= "https://www.amazon.com.au/"
 #navigates the empty chrome window to the url specified
 driver.get(URL)
 
+#wait so the website can load (possible problem with slower connections.)
+time.sleep(3)
+
 # find search bar to start searches
 searchBar = driver.find_element(By.ID, "twotabsearchtextbox")
 
@@ -43,13 +46,15 @@ searchBar.send_keys(Keys.ENTER)
 
 #%% Collect products from the page
 
+#wait so the website can load (possible problem with slower connections.)
+time.sleep(2)
+
 # XPATH for all the chunks that contain the hrefs
 #//*[@class="a-link-normal s-no-outline"]
-
 # find all the products on the page by using an XPATH search to find all 
 products = driver.find_elements(By.XPATH, "//*[@class='a-link-normal s-no-outline']")
 
-#%%  collect products from page
+#%% Collect products from page
 
 # this list will contain all the hyperlink references (hrefs) we find on the pages
 hrefs = []
@@ -69,6 +74,46 @@ for prod in products:
 #%% go to product page
 
 
+#loop through all the products
+for link in hrefs:
+    #Loads the product page using hyperlink from the resulkts page
+    driver.get(link)
+    #collect name of product
+    name = driver.find_element(By.ID, "productTitle").text
+    print(name)
+    
+    #collect ingredients
+    #ingredients = driver.find_element()
+    #print(ingredients)
+    
+    #find seller
+    # //*[@class="tabular-buybox-container"] top box
+    # //*[@class="tabular-buybox-text"][2] more specific
+    seller = driver.find_element(By.XPATH, "//*[@class='tabular-buybox-text'][2]").text
+    if seller == "":
+        #get seller a different way becuase amazon offers recurring purchase options and thus has a different layout.
+        seller = driver.find_element(By.XPATH, "//*[@id='sfsb_accordion_head']//div[2]//div//span[2]").text
+    print(seller)
+    
+    #collect product infromation under the "about this item" section
+    writtentext = []
+    featuresList = driver.find_elements(By.XPATH, "//*[@id='feature-bullets']//li/span")
+    for ft in featuresList:
+        writtentext.append(ft.text)
+    writtentext.append(driver.find_element(By.XPATH, "//*[@id='productDescription']/p/span").text)
+    for text in writtentext:
+        print(text)
+    
+    #manufacturer
+    manufacturer = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[3]/span/span[2]").text
+    print(manufacturer)
+
+    #ASIN
+    asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
+    print(asin)
+
+    print("-------------------------------------")
+    time.sleep(3)
 
 
 #%% collect image from page

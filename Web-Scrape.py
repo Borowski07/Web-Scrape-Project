ee12457dce598f881ab83f140a9f63264ed3badf
Matthew Borowski <9100606+Borowski07@users.noreply.github.com>
2023-03-17 19:50:43 +1100

    Collect ingreds, date, keyword, updates to others.

--- Web-Scrape.py
+++ Web-Scrape.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+from datetime import datetime
 
 #%% Selenium Driver Setup (chrome window)
 
@@ -73,18 +74,46 @@ for prod in products:
 
 #%% go to product page
 
-
 #loop through all the products
 for link in hrefs:
     #Loads the product page using hyperlink from the resulkts page
     driver.get(link)
+
+    time.sleep(3)
+
     #collect name of product
     name = driver.find_element(By.ID, "productTitle").text
     print(name)
     
+    ingredName = "Ingredients:"
     #collect ingredients
-    #ingredients = driver.find_element()
-    #print(ingredients)
+    try:
+        #section = driver.find_element(By.ID, "important-information")
+        #ingredients = driver.find_element()
+        #find what section has the ingredients, its nmot always in the same position
+        infoSections = driver.find_elements(By.XPATH, "//*[@id='important-information']/div/h4")
+        
+        #for infoSec in infoSections:
+        #    print(infoSec.text)
+        i = 0
+        ingredients = ""
+        for infoSec in infoSections:
+            i += 1
+            if infoSec.text == ingredName:
+                if i == "1":
+                    ingredients = driver.find_element(By.XPATH, "//*[@id='important-information']/div[1]/p").text
+                elif i == "2":
+                    ingredients = driver.find_element(By.XPATH, "//*[@id='important-information']/div[2]/p").text
+                else:
+                    ingredients = driver.find_element(By.XPATH, "//*[@id='important-information']/div[3]/p").text
+                #position of ingredients in list at i index
+                #//*[@id='important-information']/div[i]/p[2]
+                #dynamicXPath = "//*[@id='important-information']/div[" + i + "]/p"
+                #print(dynamicXPath)
+                #ingredients = driver.find_elements(By.XPATH, dynamicXPath).text
+                print(ingredients)
+    except:
+        print("---- no ingredients listed ----")
     
     #find seller
     # //*[@class="tabular-buybox-container"] top box
@@ -100,20 +129,68 @@ for link in hrefs:
     featuresList = driver.find_elements(By.XPATH, "//*[@id='feature-bullets']//li/span")
     for ft in featuresList:
         writtentext.append(ft.text)
-    writtentext.append(driver.find_element(By.XPATH, "//*[@id='productDescription']/p/span").text)
+    #if a description is available
+    try:    
+        description = driver.find_element(By.XPATH, "//*[@id='productDescription']/p/span")
+        writtentext.append(description.text)
+    except:
+        print("---- no description section ----")
+
     for text in writtentext:
         print(text)
     
-    #manufacturer
-    manufacturer = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[3]/span/span[2]").text
+    #manufacturer and ASIN
+    #
+    details = driver.find_elements(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li/span/span")
+    i = 0
+    asin = ""
+    manufacturer = ""
+    for dets in details:
+        #print(dets.text)
+        i += 1
+        editDets = dets.text.rstrip(" :")
+        #print(editDets)
+        #if "Manufacturer" in dets.text :
+        if "Manufacturer" == editDets :
+            #print ("---MANUFACTURER SOMEWHAT FOUND")
+            #print(i)
+            if i == 5:
+                #print("--- found manu @ pos 3")
+                manufacturer = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[3]/span/span[2]").text
+                #manufacturer = driver.find_element(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li[3]/span/span[2]").text
+            if i == 7:
+                #print("--- found manu @ pos 4")
+                manufacturer = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
+                #manufacturer = driver.find_element(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li[4]/span/span[2]").text
+        #if "ASIN" in dets.text :
+        if "ASIN" == editDets :
+            #print ("---ASIN SOMEWHAT FOUND")
+            #print(i)
+            if i == 7:
+                #print("--- found ASIN @ pos 4")
+                asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
+                #asin = driver.find_element(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li[4]/span/span[2]").text
+            if i == 9:
+                #print("--- found ASIN @ pos 5")
+                asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[5]/span/span[2]").text
+                #asin = driver.find_element(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li[5]/span/span[2]").text
     print(manufacturer)
-
-    #ASIN
-    asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
     print(asin)
 
+    #keyword search
+    print(searchTerm)
+
+    #date collected
+    date = str(datetime.today().strftime('%Y-%m-%d'))
+    print(date)
+
+    #old way of finding ASIN
+    #asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
+    #print(asin)
+
+
     print("-------------------------------------")
-    time.sleep(3)
+    time.sleep(1)
 
 
 #%% collect image from page

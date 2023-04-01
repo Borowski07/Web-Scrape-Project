ba68753048717c413b149bfb946d9a6f5961a71e
Matthew Borowski <9100606+Borowski07@users.noreply.github.com>
2023-04-01 20:43:39 +1100

    fixed formatting and test run

--- Web-Scrape.py
+++ Web-Scrape.py
@@ -87,9 +87,26 @@ for prod in products:
 
 
 
-#%% Image collection
+#%% set up lists for collected information
 
-def imageCollection():
+productName = [] #comes from Name function
+hyperlinks = [] #comes from 'core" function
+manufacturers = [] #comes from CollectAsinManufacturer function
+sellers = [] #comes from ProductSeller function
+asins = [] #comes from CollectAsinManufacturer function
+keywords = [] #comes from 'core" function
+artgs = [] #comes from OCR_Image function
+descriptions = [] #comes from DescriptionText function
+productIngredients = [] #comes from FindIngredients function
+datesCollected = [] #comes from CollectDate fucntion
+imageProofName = [] #comes from ProductScreenshot function
+OcrResults = [] #comes from OCR_Image function
+
+
+#%% Load all function
+
+# Image collection
+def ImageCollection():
     from selenium.webdriver.common.action_chains import ActionChains
 
     thumbnails = []
@@ -119,73 +136,34 @@ def imageCollection():
         #formats the number to be a 6 digit number eg 1 will be 000001
         number = "{0:0=2d}".format(x)
         #create the output name
-        outputLocation = "Product Images/"+ "image"+ number + ".png"
+        proofName = "image"+ number + ".png"
+        outputLocation = "Product Images/"+ proofName
         
         #saving image thats found on image webpage
         with open (outputLocation, 'wb') as file:
             file.write(driver.find_element(By.XPATH, "//body/img").screenshot_as_png)    
 
-
-
-#%% delete
+# Delete images
 def Delete_Image(file):
     import os
     print(file + " deleting")
     os.remove(file)
 
-#%% OCR of images to find ARTG number using pytesseract
-def OCR_Image(filePath):
-    #import useful packages
-    import pytesseract
-    from PIL import Image
-
-    #original image, no preprocessing
-    #im_file = "Product Images/image01.png"
-    #loads image into memory
-    img = Image.open(filePath)
-    #saves the text found by OCR
-    ocr_result = pytesseract.image_to_string(img)
-    #prints all the text found
-    print(ocr_result)
-    #format text better
-    ocr_result = ocr_result.replace("\n", " ")
-    OCR_Results.append(ocr_result)
-    ARTG_L = ""
-    ARTG_R = ""
-    try:
-        startIndex = ocr_result.find("AUST L")
-        endIndex = startIndex + 13
-        ARTG_L = ocr_result[startIndex:endIndex].strip()
-    except:
-        ARTG_L = ""
-    try:
-        startIndex = ocr_result.find("AUST R")
-        endIndex = startIndex + 13
-        ARTG_R = ocr_result[startIndex:endIndex].strip()
-    except:
-        ARTG_R = ""
-    print(ARTG_L + " " +ARTG_R)
-
-
-
-#%% go to product page
-
-#Counting the number of products
-n = 0
-
-#loop through all the products
-for link in hrefs:
-    #Loads the product page using hyperlink from the resulkts page
-    driver.get(link)
-    #wait for page to load
-    time.sleep(3)
-
-    #collect name of product
+# Collect name of product
+def Name():
     name = driver.find_element(By.ID, "productTitle").text
+    productName.append(name)
     print(name)
-    
+
+# Date collected
+def CollectDate():
+    date = str(datetime.today().strftime('%Y-%m-%d'))
+    datesCollected.append(date)
+    print(date)
+
+# Collect ingredients
+def FindIngredients():
     ingredName = "Ingredients:"
-    #collect ingredients
     try:
         #section = driver.find_element(By.ID, "important-information")
         #ingredients = driver.find_element()
@@ -213,8 +191,10 @@ for link in hrefs:
                 print(ingredients)
     except:
         print("---- no ingredients listed ----")
-    
-    #find seller
+    productIngredients.append(ingredients)
+
+# Find seller
+def ProductSeller():
     # //*[@class="tabular-buybox-container"] top box
     # //*[@class="tabular-buybox-text"][2] more specific
     seller = driver.find_element(By.XPATH, "//*[@class='tabular-buybox-text'][2]").text
@@ -222,24 +202,13 @@ for link in hrefs:
         #get seller a different way becuase amazon offers recurring purchase options and thus has a different layout.
         seller = driver.find_element(By.XPATH, "//*[@id='sfsb_accordion_head']//div[2]//div//span[2]").text
     print(seller)
-    
-    #collect product infromation under the "about this item" section
-    writtentext = []
-    featuresList = driver.find_elements(By.XPATH, "//*[@id='feature-bullets']//li/span")
-    for ft in featuresList:
-        writtentext.append(ft.text)
-    #if a description is available
-    try:    
-        description = driver.find_element(By.XPATH, "//*[@id='productDescription']/p/span")
-        writtentext.append(description.text)
-    except:
-        print("---- no description section ----")
+    sellers.append(seller)
 
-    for text in writtentext:
-        print(text)
-    
-    #manufacturer and ASIN
-    #
+# Manufacturer and ASIN
+def CollectAsinManufacturer():
+    #old way of finding ASIN
+    #asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
+    #print(asin)
     details = driver.find_elements(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li/span/span")
     i = 0
     asin = ""
@@ -274,81 +243,143 @@ for link in hrefs:
                 asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[5]/span/span[2]").text
                 #asin = driver.find_element(By.XPATH, "//*[@id='detailBulletsWrapper_feature_div']/div/ul[1]/li[5]/span/span[2]").text
     print(manufacturer)
+    manufacturers.append(manufacturer)
     print(asin)
+    asins.append(asin)
 
-    #keyword search
-    print(searchTerm)
-
-    #date collected
-    date = str(datetime.today().strftime('%Y-%m-%d'))
-    print(date)
-
-    #old way of finding ASIN
-    #asin = driver.find_element(By.XPATH, "//*[@id='detailBullets_feature_div']/ul/li[4]/span/span[2]").text
-    #print(asin)
-
-    #proof screenshot
-    n += 1
+# Collect screenshot of product screen
+def ProductScreenshot(tally):
     #formats the number to be a 6 digit number eg 1 will be 000001
-    number = "{0:0=6d}".format(n)
+    number = "{0:0=6d}".format(tally)
     #create the output name
-    outputLocation = "Product Screenshots/"+ "proof"+ number + ".png"
+    screenshotName = "proof"+ number + ".png"
+    outputLocation = "Product Screenshots/"+ screenshotName
     print(outputLocation)
     driver.save_screenshot(outputLocation)
-    
+    imageProofName.append(screenshotName)
 
-    imageCollection()
-    
-    
-
-    ARTG_L = ""
-    ARTG_R = ""
+# OCR of images to find ARTG number using pytesseract
+def OCR_Image():
+    #import useful packages
+    import pytesseract
+    from PIL import Image
     import os
+
+    #collects all the results of each image in one place.
+    OCR_Results = []
+    ArtgResults = []
+
     # Get the list of all files and directories
     path = "Product Images/"
     dir_list = os.listdir(path)
 
-    print("Files and directories in '", path, "' :")
-
-    # prints all files
-    print(dir_list)
-
-    OCR_Results = []
     for file in dir_list:
         file = "Product Images/" + file
-        OCR_Image(file)
+
+        #original image, no preprocessing
+        #im_file = "Product Images/image01.png"
+        #loads image into memory
+        img = Image.open(file)
+        #saves the text found by OCR
+        ocr_result = pytesseract.image_to_string(img)
+        #prints all the text found
+        print(ocr_result)
+        #format text better
+        ocr_result = ocr_result.replace("\n", " ")
+        
+        OCR_Results.append(ocr_result)
+        ARTG_L = ""
+        ARTG_R = ""
+        try:
+            startIndex = ocr_result.find("AUST L")
+            endIndex = startIndex + 13
+            ARTG_L = ocr_result[startIndex:endIndex].strip()
+        except:
+            ARTG_L = ""
+        try:
+            startIndex = ocr_result.find("AUST R")
+            endIndex = startIndex + 13
+            ARTG_R = ocr_result[startIndex:endIndex].strip()
+        except:
+            ARTG_R = ""
+        artgFinal = ARTG_L + " " +ARTG_R
+        print(artgFinal)
+        ArtgResults.append(artgFinal)
+
         Delete_Image(file)
-        print("------- " + file + " deleted -----------")
+
+    #put all results into a single line and append product information
+    ocrOutput = ""
+    for result in OCR_Results:
+        ocrOutput = ocrOutput + result
+    OcrResults.append(ocrOutput)
+    artgOutput = ""
+    for result in ArtgResults:
+        artgOutput = artgOutput + result
+    artgs.append(artgOutput)
+
+# Collect product infromation under the "about this item" section
+def DescriptionText():
+    writtentext = []
+    featuresList = driver.find_elements(By.XPATH, "//*[@id='feature-bullets']//li/span")
+    for ft in featuresList:
+        writtentext.append(ft.text)
+    #if a description is available
+    try:    
+        description = driver.find_element(By.XPATH, "//*[@id='productDescription']/p/span")
+        writtentext.append(description.text)
+    except:
+        print("---- no description section ----")
+
+    for text in writtentext:
+        print(text)
+    textOutput = ""
+    for txt in writtentext:
+        textOutput = textOutput + txt
+    descriptions.append(textOutput)
+    
 
 
+#%% go to product page
+
+#Counting the number of products
+n = 0
+
+#loop through all the products
+for link in hrefs:
+    #Loads the product page using hyperlink from the resulkts page
+    driver.get(link)
+    hyperlinks.append(link)
+    #wait for page to load
+    time.sleep(3)
+
+    Name() #collect name of product
+    FindIngredients() #collect ingredients
+    
+    #------------------problem like this needs a solution---------------------
+    #https://www.amazon.com.au/Swisse-Ultiboost-Vitamin-0-202-Kilograms/dp/B013JKSIUG/ref=sr_1_46?keywords=Vitamins%2B%26%2BSupplements&qid=1680339856&sr=8-46&th=1
+    #---------------------------------------
+    ProductSeller() #find seller
+
+    DescriptionText() #collect product infromation under the "about this item" section
+    CollectAsinManufacturer() #manufacturer and ASIN
+
+    #keyword search
+    print(searchTerm)
+    keywords.append(searchTerm)
+
+    CollectDate() #date collected
+    ProductScreenshot(n) #proof screenshot
+    ImageCollection() #images from product thumbnails 
+    OCR_Image() #run OCR on images to extract text
+
+    
     print("-------------------------------------")
     time.sleep(1)
-
-
-#%% collect image from page
-
+    n = n + 1
 
 
 
-
-# %%
-
-import os
-# Get the list of all files and directories
-path = "Product Images/"
-dir_list = os.listdir(path)
-
-print("Files and directories in '", path, "' :")
-
-# prints all files
-print(dir_list)
-
-OCR_Results = []
-for file in dir_list:
-    file = "Product Images/" + file
-    OCR_Image(file)
-    Delete_Image(file)
-    print("------- " + file + " deleted -----------")
 
 
 #%% Output infromation to a csv file
